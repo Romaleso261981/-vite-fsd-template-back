18d15e63095b36a410453a22cf7467e94e7f271a
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-03-20 11:29:24 +0200

    some bag fix

---
+++ src/controllers/products/getAllKovaniZabori.js
@@ -0,0 +1,312 @@
+const mockdataKovaniZabori = [
+  {
+    id: "1",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "2",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "3",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "4",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "5",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "6",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "7",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "8",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "9",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "10",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "11",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "12",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "13",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "14",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "15",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "16",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "17",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "18",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "19",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "20",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "21",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "22",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "23",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "24",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "25",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "26",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "27",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "28",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "29",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "30",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "31",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "32",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  },
+  {
+    id: "",
+    title: "КОВАНІ ЗАБОРИ",
+    image: "https://images.prom.ua/1612796544_w262_h242_kovanij-zabor.jpg",
+    code: "Б-0101",
+    cost: 24200,
+    category: "kovaniZabori",
+    date: "August 18, 2022"
+  }
+];
+async function getAllKovaniZabori(req, res) {
+  try {
+    return res.status(200).json({
+      status: "success",
+      code: 200,
+      mockdataKovaniZabori
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports = getAllKovaniZabori;
--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -1,5 +1,15 @@
 const getAllProductsGroup = require("./getAllProductsGroup");
+const getAllKozirki = require("./getAllKozirki");
+const getAllKovaniGrati = require("./getAllKovaniGrati");
+const getAllKovaniZabori = require("./getAllKovaniZabori");
+const getAllEliteIronGates = require("./getAllEliteIronGates");
+const getAllGateWithCorrugatedBoard = require("./getAllGateWithCorrugatedBoard");
 
 module.exports = {
-  getAllProductsGroup
+  getAllProductsGroup,
+  getAllKozirki,
+  getAllKovaniGrati,
+  getAllKovaniZabori,
+  getAllEliteIronGates,
+  getAllGateWithCorrugatedBoard
 };
